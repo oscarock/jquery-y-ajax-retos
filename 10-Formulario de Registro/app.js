d6363d49d1c4bf653246e916e8b11f97b56748bd
Your Name <you@example.com>
2017-09-14 11:22:08 -0500

    reto 10 jquery plataforma ok :)

--- 10-Formulario de Registro/app.js
+++ 10-Formulario de Registro/app.js
@@ -1,5 +1,30 @@
 $('form').on('submit', function(e) {
   e.preventDefault(); // previene la acción por defecto (enviar el formulario)
+   if ($("#username").val() === "") {
+  	$(".username").addClass("has-error")
+  	$(".username p").html("Campo usuario no pueder estar vacio.")
+  }else if(/\s/.test($("#username").val())){
+  	$(".username").addClass("has-error")
+  	$(".username p").html("No puede tener espacio en blanco.")
+  }else{
+  	$(".username").removeClass("has-error")
+  	$(".username p").html("")
+  }
 
-  // escribe acá tu solución
+  if($("#password").val() === ""){
+  	$(".password").addClass("has-error")
+  	$(".password p").html("Campo password no pueder estar vacio.")
+  }else if($("#password").val().length < 6 || $("#password").val().length > 40){
+  	$(".password").addClass("has-error")
+  	$(".password p").html("el numero de caracteres invalido (min:6 a max:40 caracteres).")
+  }else{
+  	$(".password").removeClass("has-error")
+  	$(".password p").html("")
+  }
+ 	
+  if($("#terms").is(':checked') === false){
+  	$(".terms-checkbox").addClass("has-error")
+  }else{
+  	$(".terms-checkbox").removeClass("has-error")
+  }
 });
