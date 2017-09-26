93580a2c0bec7d2bae5082ac50ba989d6067fc14
Your Name <you@example.com>
2017-09-26 08:48:33 -0500

    Proyecto final terminado

--- proyecto-final/app.js
+++ proyecto-final/app.js
@@ -3,7 +3,6 @@ console.log(val)
 var number_load = val.toString().split("") 
 
 $("form").on("submit", function(e){
-	console.log($("#number").val().repeat(0))
   e.preventDefault()
   e.stopPropagation();
 
@@ -22,44 +21,32 @@ $("form").on("submit", function(e){
   }else if($("#number").val().length === 4){
   	$("#number").removeClass("is-invalid").addClass("is-valid")
   	$(".error-text").hide()
+	  var number_input = $("#number").val().split("")
+		var sum_input_picas = 0
+		var sum_input_fijas = 0
+
+		for(var i = 0; i < number_load.length; i++){
+	    for(var j = 0; j < number_input.length; j++){
+	      if(number_load[i] == number_input[j]){
+	        if(i === j){
+	        	sum_input_fijas += number_load[i].length
+	        }else{
+	        	sum_input_picas += number_load[i].length
+					}
+	  		}		
+	    }
+	 	}
+	 	
+		console.log("Picas: " + sum_input_picas)
+		console.log("Fijas: " + sum_input_fijas)
+		if(sum_input_fijas === 4){
+			$('#Mymodal').modal()
+		}
+
+		$("#tabla-history").append("<tr><td>"+ $("#number").val() +"</td><td>"+ sum_input_picas +"</td><td>"+ sum_input_fijas +"</td></tr>")
+
+		$("#btn-play").on("click",function(){
+			window.location.reload()
+		})
   }
-
-   // var expression = /[0-9]{4}/.exec($("#number").val())
-
-   //  if(expression != null){
-   //      return true
-   //  }else{
-   //      return false
-   //  }
- //  var number_input = $("#number").val().split("")
-	// var sum_input_picas = 0
-	// var sum_input_fijas = 0
-
-	// for(var i = 0; i < number_load.length; i++){
- //    for(var j = 0; j < number_input.length; j++){
- //      if(number_load[i] == number_input[j]){
- //        if(i === j){
- //        	sum_input_fijas += number_load[i].length
- //        	// result = "Fijas: " + sum_input_fijas
- //            // console.log('Se repite el numero : ' + number_load[i] + ' en la posición '+ (i + 1))
- //        }else{
- //        	sum_input_picas += number_load[i].length
- //        	// result = "Picas: " + sum_input_picas
- //        	// console.log('Se repite el numero : ' + number_load[i] + ' pero su posicion no coincide ')
-	// 			}
- //  		}		
- //    }
- // 	}
- // 	// return result
-	// console.log("Picas: " + sum_input_picas)
-	// console.log("Fijas: " + sum_input_fijas)
-	// if(sum_input_fijas === 4){
-	// 	$('#Mymodal').modal()
-	// }
-
-	// $("#tabla-history").append("<tr><td>"+ $("#number").val() +"</td><td>"+ sum_input_picas +"</td><td>"+ sum_input_fijas +"</td></tr>")
-
-	// $("#btn-play").on("click",function(){
-	// 	window.location.reload()
-	// })
 })
